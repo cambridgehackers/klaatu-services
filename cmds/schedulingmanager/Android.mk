6d20ef4f8c4b63cac6942fc954b954f7e601c0ea
John Ankcorn <jca@alum.mit.edu>
2012-10-11 16:41:10 -0400

    added necessary library

--- cmds/schedulingmanager/Android.mk
+++ cmds/schedulingmanager/Android.mk
@@ -4,8 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= schedulingmanager_main.cpp
 LOCAL_MODULE:= schedulingmanager
 LOCAL_MODULE_TAGS:=optional
+LOCAL_C_INCLUDES := frameworks/av/services/audioflinger
 
-LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils 
+LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils
+LOCAL_STATIC_LIBRARIES := libscheduling_policy
 
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/bin/schedulingmanager
 include $(BUILD_EXECUTABLE)
