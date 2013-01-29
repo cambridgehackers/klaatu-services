c2254a038b455b068ddf3a0c4b8973e2f54196cb
John Ankcorn <jca@alum.mit.edu>
2013-01-29 16:35:17 -0500

    schedulingmanager not needed on 4.0.4

--- cmds/schedulingmanager/Android.mk
+++ cmds/schedulingmanager/Android.mk
@@ -9,5 +9,9 @@ LOCAL_C_INCLUDES := frameworks/av/services/audioflinger
 LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils
 LOCAL_STATIC_LIBRARIES := libscheduling_policy
 
+ifneq ($(PLATFORM_VERSION),4.0.0)
+ifneq ($(PLATFORM_VERSION),4.0.4)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/bin/klaatu_schedulingmanager
 include $(BUILD_EXECUTABLE)
+endif
+endif
