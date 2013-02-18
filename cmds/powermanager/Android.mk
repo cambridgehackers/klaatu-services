458298aa58825bc3899243b4d8f60317ea284a6e
John Ankcorn <jca@alum.mit.edu>
2013-02-18 09:00:28 -0500

    added support for 2.3.7

--- cmds/powermanager/Android.mk
+++ cmds/powermanager/Android.mk
@@ -1,5 +1,6 @@
 ifneq ($(PLATFORM_VERSION),2.3.3)
 ifneq ($(PLATFORM_VERSION),2.3.6)
+ifneq ($(PLATFORM_VERSION),2.3.7)
 #doesn't work on 2.3
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -12,5 +13,6 @@ LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils libpowermanager
 
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/bin/klaatu_powermanager
 include $(BUILD_EXECUTABLE)
+endif
 endif
 endif
