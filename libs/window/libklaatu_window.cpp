44dab356d11e55ba2dc1ec1f21f56da12e4cae98
Dmitriy Cherkasov <@>
2014-02-10 16:54:48 -0800

    libs/window: add custom resolution support

--- libs/window/libklaatu_window.cpp
+++ libs/window/libklaatu_window.cpp
@@ -27,12 +27,17 @@ using android::sp;
 
 class klaatu_video {
     public:
-	static unsigned int get_native_window();
+	static unsigned int get_native_window(int w, int h);
+	static void get_display_size(int* w, int* h);
 };
 
 extern "C" {
-	unsigned int klaatu_get_window() {
-		return klaatu_video::get_native_window();
+	unsigned int klaatu_get_window(int w, int h) {
+		return klaatu_video::get_native_window(w, h);
+	}
+
+	void klaatu_get_display_size(int* w, int* h){
+		return klaatu_video::get_display_size(w, h);
 	}
 };
 
@@ -43,7 +48,31 @@ static sp<android::SurfaceComposerClient> mSession;
 static sp<android::SurfaceControl> mControl;
 static sp<android::Surface> mAndroidSurface;
 
-unsigned int klaatu_video::get_native_window()
+void klaatu_video::get_display_size(int* w, int* h)
+{
+	android::DisplayInfo dinfo;
+	mSession = new android::SurfaceComposerClient();
+
+	#if defined(SHORT_PLATFORM_VERSION) && (SHORT_PLATFORM_VERSION >= 42)
+		sp<android::IBinder> dtoken(android::SurfaceComposerClient::getBuiltInDisplay(android::ISurfaceComposer::eDisplayIdMain));
+		int status = mSession->getDisplayInfo(dtoken, &dinfo);
+	#else
+		int status = mSession->getDisplayInfo(0, &dinfo);
+	#endif
+
+	if (status)
+		{
+		w = h = 0;
+		return;
+		}
+
+	w = dinfo.w;
+	h = dinfo.h;
+
+	return;
+}
+
+unsigned int klaatu_video::get_native_window(int w, int h)
 {
 	android::DisplayInfo dinfo;
 	mSession = new android::SurfaceComposerClient();
@@ -54,10 +83,16 @@ unsigned int klaatu_video::get_native_window()
 #else
 	int status = mSession->getDisplayInfo(0, &dinfo);
 #endif
-	
+
 	if (status)
-        	return 0;  
-  
+        	return 0;
+
+	if (w <= 0)
+		w = dinfo.w;
+
+	if (h <= 0)
+		h = dinfo.h;
+
 	mControl = mSession->createSurface(
 #if defined(SHORT_PLATFORM_VERSION) && (SHORT_PLATFORM_VERSION >= 42)
 		android::String8("libklaatuwindow"),
@@ -66,8 +101,8 @@ unsigned int klaatu_video::get_native_window()
 #else
 		0,
 #endif
-		dinfo.w, dinfo.h, android::PIXEL_FORMAT_RGB_888, 0);
-  
+		w, h, android::PIXEL_FORMAT_RGB_888, 0);
+
 	android::SurfaceComposerClient::openGlobalTransaction();
 	mControl->setLayer(0x40000000);
 	android::SurfaceComposerClient::closeGlobalTransaction();
--- libs/window/libklaatu_window.h
+++ libs/window/libklaatu_window.h
@@ -3,6 +3,8 @@
 #define VALUE_TO_STRING(x) #x
 #define VALUE(x) VALUE_TO_STRING(x)
 #define VAR_NAME_VALUE(var) #var "="  VALUE(var)
-unsigned int klaatu_get_window(void);
+unsigned int klaatu_get_window(int w, int h);
+void klaatu_get_display_size(int* w, int* h);
+
 
 #endif
